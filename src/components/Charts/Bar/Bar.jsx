a9f7ddc36440b25a11b5c3dafe025c2ada564a03
Sergey Parshukov <sp@frank.ly>
2018-09-06 22:55:42 +0300

    Refactored bar chart

--- src/components/Charts/Bar/Bar.jsx
+++ src/components/Charts/Bar/Bar.jsx
@@ -10,19 +10,16 @@ import {
   XAxis,
 } from 'recharts'
 import { injectStyles } from '@frankmoney/ui'
-import { formatCurrency, Paper } from '@frankmoney/components'
-import IconCircle from 'components/IconCircle'
+import Grid from './Grid'
+import Tick from './Tick'
+import Tooltip from './Tooltip'
+import { epsilon } from './TooltipLine'
 
 const BAR_CORNER_RADIUS = 3
-const BASE_LINE_COLOR = '#E5E5E5'
 const BASE_LINE_OFFSET = 3
-const CLIPPING_FIX = 1
-const DASH_LINE_COLOR = '#EBEBEB'
 const DEFAULT_VALUE_PROP = 'value'
 const FOOTER_PADDING = 20
 const FOOTER_TEXT_HEIGHT = 31
-const LEGEND_COLOR = '#808080'
-const LEGEND_SECOND_COLOR = '#B3B3B3'
 const NEGATIVE_VALUE_PROP = 'negativeValue'
 const PADDING = 20
 export const POSITIVE_BAR_COLOR = '#21CB61'
@@ -31,166 +28,26 @@ export const PRIMARY_BAR_COLOR = '#484DE7'
 const WIDTH = 790
 const HEIGHT = 260
 
-const styles = theme => ({
+const styles = {
   root: {
     position: 'relative',
   },
   chart: {
     margin: '0 auto',
   },
-  grid: {
-    position: 'absolute',
-    top: -CLIPPING_FIX,
-  },
-  tooltip: {
-    padding: [14, 19, 9],
-    border: '1px solid rgba(0, 0, 0, 0.02)',
-    boxShadow: '0px 5px 10px rgba(0, 0, 0, 0.1)',
-  },
-  tooltipHeader: {
-    ...theme.fontRegular(14, 22),
-    color: '#999999',
-    marginBottom: 12,
-  },
-  tooltipItem: {
-    ...theme.fontMedium(14, 16),
-    display: 'flex',
-    justifyContent: 'space-between',
-    marginBottom: 10,
-  },
-  circle: {
-    position: 'relative',
-    top: 1,
-  },
-  tooltipItemText: {
-    margin: [0, 16, 0, 10],
-  },
   positiveBars: {
     transform: `translateY(-${BASE_LINE_OFFSET}px)`,
   },
-})
+}
 
-const DashedLine = ({ y, width }) => (
-  <line
-    x1="0"
-    x2={width}
-    y1={y}
-    y2={y}
-    stroke={DASH_LINE_COLOR}
-    strokeWidth={1}
-    strokeDasharray="2"
-  />
-)
-
-// TODO: move out
-const Grid = injectStyles(styles)(
-  ({ className, dual, hideBaseLine = false, steps = 4, height, width }) => {
-    const baseLine = dual ? Math.floor(height / 2) : height
-    const dashedLines = []
-    const step = Math.floor(baseLine / steps)
-    // eslint-disable-next-line no-plusplus
-    for (let i = 1; i <= steps; i++) {
-      dashedLines.push(
-        <DashedLine
-          key={i}
-          y={baseLine - i * step + CLIPPING_FIX}
-          width={width}
-        />
-      )
-      if (dual) {
-        dashedLines.push(
-          <DashedLine
-            key={steps + i}
-            y={baseLine + i * step + CLIPPING_FIX}
-            width={width}
-          />
-        )
-      }
-    }
-
-    return (
-      <svg
-        width={width}
-        height={height + 2 * CLIPPING_FIX}
-        className={className}
-      >
-        {dashedLines}
-        {!hideBaseLine && (
-          <line
-            x1="0"
-            x2={width}
-            y1={baseLine}
-            y2={baseLine}
-            strokeWidth={1}
-            stroke={BASE_LINE_COLOR}
-          />
-        )}
-      </svg>
-    )
-  }
-)
-
-const epsilon = 2 ** -42
 const negateWithEpsilon = x => (x === 0 ? -epsilon : -x)
 const fixNegative = R.over(R.lensProp('negativeValue'), negateWithEpsilon)
-const fixMinusZero = x => (x === -epsilon ? 0 : x)
 
-const formatMoney = R.pipe(
-  fixMinusZero,
-  x => ({ value: x, precision: 2 }),
-  formatCurrency,
-  R.replace('$', '$ '),
-  R.replace('– ', '−')
-)
-
-const TooltipLine = injectStyles(styles)(
-  ({ caption, classes, dataKey, color, key, value }) => (
-    <div className={classes.tooltipItem} style={{ color }} key={key}>
-      <div>
-        <IconCircle className={classes.circle} />
-        <span className={classes.tooltipItemText}>
-          {caption || (dataKey === NEGATIVE_VALUE_PROP ? 'Spending' : 'Income')}
-        </span>
-      </div>
-      {formatMoney(value)}
-    </div>
-  )
-)
-
-export const Tooltip = injectStyles(styles)(
-  ({ caption, classes, label, payload, style }) => (
-    <Paper className={classes.tooltip} style={style}>
-      <div className={classes.tooltipHeader}>{label}</div>
-      {R.map(x => <TooltipLine caption={caption} {...x} />)(payload)}
-    </Paper>
-  )
-)
-
-const Tick = ({ x, y, payload: { value } }) => {
-  const [text, secondLine] = R.split(' ', value)
-  return (
-    <>
-      <text x={x} y={y} textAnchor="middle" fontSize={12} fill={LEGEND_COLOR}>
-        {text}
-      </text>
-      {secondLine && (
-        <text
-          x={x}
-          y={y + 22}
-          textAnchor="middle"
-          fontSize={12}
-          fill={LEGEND_SECOND_COLOR}
-        >
-          {secondLine}
-        </text>
-      )}
-    </>
-  )
-}
+const tooltipLabelFormatter = payload =>
+  payload.dataKey === NEGATIVE_VALUE_PROP ? 'Spending' : 'Income'
 
 const BarChart = ({
   barColor,
-  caption,
   classes,
   className,
   data,
@@ -212,7 +69,6 @@ const BarChart = ({
   return (
     <div className={cx(classes.root, className)} style={{ width, height }}>
       <Grid
-        className={classes.grid}
         dual={dual}
         width={width}
         steps={dual ? 2 : 4}
@@ -241,7 +97,7 @@ const BarChart = ({
           tickSize={4}
         />
         <ReTooltip
-          content={<Tooltip caption={caption} />}
+          content={<Tooltip labelFormatter={tooltipLabelFormatter} />}
           isAnimationActive={false}
           cursor={false}
         />
@@ -274,7 +130,6 @@ const BarChart = ({
 
 BarChart.propTypes = {
   barColor: PropTypes.string,
-  caption: PropTypes.string,
   data: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string,
