a9cd6536b4e43d10685354eeb39a81464b23e799
Sergey Parshukov <sp@frank.ly>
2018-11-15 02:22:33 +0300

    Somewhat typed injectStyles. Cleaner flowconfig

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -1,8 +1,33 @@
-// @flow strict-local
-// eslint-disable-next-line import/prefer-default-export
-export { injectStyles } from '@frankmoney/ui'
+// @flow strict
+import * as React from 'react'
+import { injectStyles as originInjectStyles } from '@frankmoney/ui'
 
-type Classes = { [class: string]: string }
+type Styles = Object // flowlint-line unclear-type:off
+
+type Theme = Object // flowlint-line unclear-type:off
+type StyleCreator = (theme: Theme) => Styles
+
+type InjectOptions = {|
+  fixedGrid?: boolean,
+  grid?: boolean,
+  inject?: Array<string>,
+|}
+
+// TODO: better types, not sure if possible with flow
+type ReactComponent = React.ComponentType<any> // flowlint-line unclear-type:off
+type StyledComponent = React.ComponentType<any> // flowlint-line unclear-type:off
+
+type PropInjector = (component: ReactComponent) => StyledComponent
+
+type InjectStylesFn = (
+  stylesOrCreator: Styles | StyleCreator,
+  options?: InjectOptions
+) => PropInjector
+
+// eslint-disable-next-line import/prefer-default-export
+export const injectStyles: InjectStylesFn = originInjectStyles
+
+export type Classes = { [class: string]: string }
 
 export type InjectStylesProps = {|
   classes: Classes,
