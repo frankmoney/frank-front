ea07f7a3b1216ac397c3599a40f6c941d3656634
Sergey Parshukov <sp@frank.ly>
2018-11-23 20:42:09 +0300

    Fixed missing filter parameters

--- src/containers/admin/Ledger/epics/barsZoom.js
+++ src/containers/admin/Ledger/epics/barsZoom.js
@@ -1,13 +1,22 @@
 import FILTERS_ACTIONS from 'containers/admin/Filters/actions'
 import * as ACTIONS from '../actions'
+import { currentFiltersSelector } from '../selectors'
 
-export default action$ =>
+export default (action$, store) =>
   action$
     .ofType(ACTIONS.barZoomIn)
-    .map(({ payload: { dateFrom, dateTo } }) => ({
-      dateLimit: {
-        from: dateFrom,
-        to: dateTo,
-      },
-    }))
+    .map(({ payload: { dateFrom, dateTo } }) => {
+      const filters = currentFiltersSelector(store.getState())
+      return {
+        sumLimit: {
+          min: filters.amountMin,
+          max: filters.amountMax,
+        },
+        verified: filters.verified,
+        dateLimit: {
+          from: dateFrom,
+          to: dateTo,
+        },
+      }
+    })
     .map(FILTERS_ACTIONS.apply)
