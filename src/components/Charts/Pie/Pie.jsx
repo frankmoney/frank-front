4117635aea1b7cfab9b2d947d692db526ee0e6e0
Sergey Parshukov <sp@frank.ly>
2018-11-28 20:29:58 +0300

    Refactored Charts/Pie

--- src/components/Charts/Pie/Pie.jsx
+++ src/components/Charts/Pie/Pie.jsx
@@ -2,15 +2,13 @@
 import * as R from 'ramda'
 import React from 'react'
 import cx from 'classnames'
-import { Cell, PieChart as ReChart, Pie as RePie } from 'recharts'
-import { injectStyles } from '@frankmoney/ui'
-import type { Props, PieSliceProps, FatPieSliceProps } from './Pie.flow'
+import { PieChart as ReChart, Pie as RePie } from 'recharts'
+import { injectStyles, type InjectStylesProps } from 'utils/styles'
+import FatPieSlice from './FatPieSlice'
+import PieSlice from './PieSlice'
 
-const DEFAULT_COLOR = '#B3B3B3'
 const INNER_RING_THICCNESS = 15
 const RING_THICCNESS = 5
-const SEGMENTS_PADDING_ANGLE = 0.25
-const SIZE = 350
 
 const styles = {
   ring: {
@@ -21,25 +19,43 @@ const styles = {
   },
 }
 
-const PieSlice = ({ color = DEFAULT_COLOR, index }: PieSliceProps) => (
-  <Cell fill={color} key={index} />
-)
+type SegmentIndex = number
 
-const FatPieSlice = ({
-  color = DEFAULT_COLOR,
-  active,
-  index,
-}: FatPieSliceProps) => <Cell fill={active ? color : 'none'} key={index} />
+type PieItem = {|
+  color?: string,
+  name: string,
+  value: number,
+|}
+
+export type PieData = Array<PieItem>
+
+type Props = {|
+  ...InjectStylesProps,
+  //
+  activeSegmentIndex?: number,
+  data: PieData,
+  size: number,
+  onSegmentMouseEnter?: SegmentIndex => void,
+  onSegmentMouseLeave?: () => void,
+|}
 
 const injectActive = current => item =>
   R.assoc('active', current === null || R.propEq('index', current, item), item)
+
+const COMMON_PIE_PROPS = {
+  dataKey: 'value',
+  endAngle: -630,
+  isAnimationActive: false,
+  nameKey: 'name',
+  paddingAngle: 0.25,
+  startAngle: -270,
+}
 
 const Pie = ({
   activeSegmentIndex,
   classes,
   className,
   data,
-  // onClick,
   onSegmentMouseEnter,
   onSegmentMouseLeave,
   size,
@@ -50,39 +66,26 @@ const Pie = ({
   const handlePieEnter =
     onSegmentMouseEnter && (({ index }) => onSegmentMouseEnter(index))
 
+  const pieProps = {
+    ...COMMON_PIE_PROPS,
+    data,
+    outerRadius,
+  }
+
   return (
     <div className={className}>
       <ReChart
         height={size}
         margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
         width={size}
-        // onClick={onClick}
       >
-        <RePie
-          className={classes.ring}
-          data={data}
-          dataKey="value"
-          endAngle={-630}
-          innerRadius={innerRadius}
-          isAnimationActive={false}
-          nameKey="name"
-          outerRadius={outerRadius}
-          paddingAngle={SEGMENTS_PADDING_ANGLE}
-          startAngle={-270}
-        >
+        <RePie className={classes.ring} innerRadius={innerRadius} {...pieProps}>
           {R.map(PieSlice, data)}
         </RePie>
         <RePie
           className={cx(classes.ring, classes.innerRing)}
-          data={data}
-          dataKey="value"
-          endAngle={-630}
           innerRadius={innerRadius - INNER_RING_THICCNESS}
-          isAnimationActive={false}
-          nameKey="name"
-          outerRadius={outerRadius}
-          paddingAngle={SEGMENTS_PADDING_ANGLE}
-          startAngle={-270}
+          {...pieProps}
         >
           {R.map(
             R.pipe(
@@ -93,16 +96,11 @@ const Pie = ({
           )}
         </RePie>
         <RePie
-          data={data}
-          dataKey="value"
-          endAngle={-630}
           innerRadius={0}
           onMouseEnter={handlePieEnter}
           onMouseLeave={onSegmentMouseLeave}
           opacity={0}
-          outerRadius={outerRadius}
-          paddingAngle={SEGMENTS_PADDING_ANGLE}
-          startAngle={-270}
+          {...pieProps}
         />
       </ReChart>
     </div>
@@ -110,7 +108,7 @@ const Pie = ({
 }
 
 Pie.defaultProps = {
-  size: SIZE,
+  size: 350,
 }
 
 export default injectStyles(styles)(Pie)
