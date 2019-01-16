2d426c7938ff5696b5e16d8a150c6ef6d902bfaa
Nick Delitski <ndelitski@gmail.com>
2019-01-16 12:52:06 +0300

    test relative path to asset subdirectories(widgets)

--- webpack.config.widget.common.js
+++ webpack.config.widget.common.js
@@ -50,7 +50,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: 'fonts/[name].[ext]',
+              name: './fonts/[name].[ext]',
             },
           },
         ],
@@ -62,7 +62,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: 'img/[name].[ext]',
+              name: './img/[name].[ext]',
             },
           },
         ],
