c78aed23812afe9e86acdc1aaf09ab004f394e55
Nick Delitski <ndelitski@gmail.com>
2019-01-24 23:59:35 +0300

    clean multiselect when leaving page

--- src/containers/admin/MultiEditSnack/MultiEditSnack.jsx
+++ src/containers/admin/MultiEditSnack/MultiEditSnack.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
+import reconnect from 'utils/reconnect'
 import MultiEditResultSnack from './MultiEditResultSnack'
 import MultiEditSelectionSnack from './MultiEditSelectionSnack'
 import MultiEditConfirmDialog from './MultiEditConfirmDialog'
 import MultiEditPaymentDialog from './MultiEditPaymentDialog'
+import ACTIONS from './actions'
 
-const MultiEditSnack = ({ categories }) => (
-  <>
-    <MultiEditPaymentDialog categories={categories} />
-    <MultiEditConfirmDialog />
-    <MultiEditResultSnack id="result" />
-    <MultiEditSelectionSnack id="selection" />
-  </>
-)
+class MultiEditSnack extends React.Component {
+  componentWillUnmount() {
+    this.props.leave()
+  }
 
-export default MultiEditSnack
+  render() {
+    const { categories } = this.props
+
+    return (
+      <>
+        <MultiEditPaymentDialog categories={categories} />
+        <MultiEditConfirmDialog />
+        <MultiEditResultSnack />
+        <MultiEditSelectionSnack />
+      </>
+    )
+  }
+}
+
+export default reconnect(null, { leave: ACTIONS.leave })(MultiEditSnack)
