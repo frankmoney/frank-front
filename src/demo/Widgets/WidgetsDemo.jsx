211e6ee072677afe121ac22ad8ec5413e1e3019f
Nick Delitski <ndelitski@gmail.com>
2019-03-30 15:13:31 +0300

    fix widgets demo private account

--- src/demo/Widgets/WidgetsDemo.jsx
+++ src/demo/Widgets/WidgetsDemo.jsx
@@ -14,49 +14,51 @@ const styles = {
   },
 }
 
+const PUBLIC_ACCOUNT_ID = 1
+
 const WidgetsDemo = ({ classes }) => (
   <Demo className={classes.demo}>
     <h2 className={classes.buttonDemo}>Button widget ➘</h2>
-    <ButtonWidget accountId={18} />
+    <ButtonWidget accountId={PUBLIC_ACCOUNT_ID} zIndex={100} />
 
     <h2>Inline widget 280x190</h2>
     <Row centered>
-      <InlineWidget accountId={4} width={280} />
+      <InlineWidget accountId={PUBLIC_ACCOUNT_ID} width={280} />
     </Row>
 
     <h2>Inline widget 350px</h2>
     <Row centered>
-      <InlineWidget accountId={4} width={350} />
+      <InlineWidget accountId={PUBLIC_ACCOUNT_ID} width={350} />
     </Row>
 
     <h2>Inline widget 400x275</h2>
     <Row centered>
-      <InlineWidget accountId={4} width={400} />
+      <InlineWidget accountId={PUBLIC_ACCOUNT_ID} width={400} />
     </Row>
 
     <h2>Inline widget 450px</h2>
     <Row centered>
-      <InlineWidget accountId={4} width={450} />
+      <InlineWidget accountId={PUBLIC_ACCOUNT_ID} width={450} />
     </Row>
 
     <h2>Inline widget 500x345</h2>
     <Row centered>
-      <InlineWidget accountId={4} width={500} />
+      <InlineWidget accountId={PUBLIC_ACCOUNT_ID} width={500} />
     </Row>
 
     <h2>Inline widget 625x430</h2>
     <Row centered>
-      <InlineWidget accountId={4} width={625} />
+      <InlineWidget accountId={PUBLIC_ACCOUNT_ID} width={625} />
     </Row>
 
     <h2>Inline widget 700px</h2>
     <Row centered>
-      <InlineWidget accountId={4} width={700} />
+      <InlineWidget accountId={PUBLIC_ACCOUNT_ID} width={700} />
     </Row>
 
     <h2>Inline widget 800x550</h2>
     <Row centered>
-      <InlineWidget accountId={4} width={800} />
+      <InlineWidget accountId={PUBLIC_ACCOUNT_ID} width={800} />
     </Row>
   </Demo>
 )
